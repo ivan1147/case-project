b094804e98c278fa18800a16ffcd71dc1f19f12c
theveron <theveron8@gmail.com>
2017-11-07 13:57:50 +0800

    Retrieve data
    competition_manage.php
    competition_form.php

--- scripts/competition_form_manage.php
+++ scripts/competition_form_manage.php
@@ -1,101 +1,113 @@
-	<?php include "head.php"?>
+<?php 
+include "head.php"
+?>
+
+<body>
+
+<div class="container">
+  <!--Navigation Bar-->
+	<?php include "navigation.php"?>
+	<br><br>
+
+  <?php 
+  //get row id from previous page
+  if(isset($_GET['id'])){
+    $competitionId = $_GET['id'];
+  	//echo $competitionId;
 	
-	<body>
+
+  //Create sql statement
+  $queryCompetition = "SELECT * FROM competition WHERE competitionId = $competitionId";
+  $queryForm = "SELECT * FROM form INNER JOIN competition ON form.competitionId = competition.competitionId WHERE form.competitionId = $competitionId";
+  //execute query
+  $queryResultCompetition = mysqli_query($conn, $queryCompetition) or die (mysqli_connect_error());
+  $queryResultForm = mysqli_query($conn, $queryForm) or die (mysqli_connect_error());
+  //check result rows
+  $numResultCompetition = mysqli_num_rows($queryResultCompetition);
+  $numResultForm = mysqli_num_rows($queryResultForm);
   
+  if ($numResultCompetition > 0 ) {
+	if ($row= mysqli_fetch_assoc($queryResultCompetition)){
+		$competitionTitle = $row['title']; 
+	}
+
+	?>
+	<!--Card-->
 	<div class="container">
-		
-		<!--Navigation Bar-->
-		<?php include "navigation.php"?>
-		
-		<br><br>
-		
-		
-		<!--Card-->
-		<div class="container">
-			
-			<h1> 10-14 Age</h1>
-			
-			<ul class="breadcrumb">
-			  <li class="breadcrumb-item"><a href="<?php echo 'home.php' ?>">Home</a></li>
-			  <li class="breadcrumb-item"><a href="<?php echo 'competition_manage.php' ?>">Manage Competition</a></li>
-			  <li class="breadcrumb-item active">10-14 Age</li>
-			</ul>
-			
-			
-			<div class="container">
-				
-				<div class="table-responsive">          
-					<table class="table">
-						<thead>
-						  <tr>
-							<th>ID</th>
-							<th>Title</th>
-							<th>Status</th>
-							<th>Created On</th>
-							<th id="operationHeader">Operation</th>
-						  </tr>
-						</thead>
-						<tbody>
-						  <tr>
-							<td>1</td>
-							<td>1st Grade</td>
-							<td>Ongoing</td>
-							<td>2017-10-24 23:11:45</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-						<tbody>
-						  <tr>
-							<td>2</td>
-							<td>2nd Grade</td>
-							<td>Ongoing</td>
-							<td>2017-10-24 12:02:31</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-						<tbody>
-						  <tr>
-							<td>3</td>
-							<td>3rd Grade</td>
-							<td>Available</td>
-							<td>2017-10-24 11:05:19</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton"  type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-						<tbody>
-						  <tr>
-							<td>4</td>
-							<td>4th Grade</td>
-							<td>Available</td>
-							<td>2017-10-24 11:14:22</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-					</table>
-					
-				
-					
-				</div>
-			
-				
-				<div class="row">		
-					<a id="createItem" href="<?php echo 'discussion_create.php' ?>" class="btn btn-info mt-3">Create Competition</a>
+	<h1><?php echo $competitionTitle?></h1>
+
+	<ul class="breadcrumb mb-0">
+		<li class="breadcrumb-item"><a href="<?php echo 'home.php' ?>">Home</a></li>
+		<li class="breadcrumb-item"><a href="<?php echo 'competition_manage.php' ?>">Manage Competition</a></li>
+		<li class="breadcrumb-item active"><?php echo $competitionTitle?></li>
+	</ul>
+
+		<?php
+		//fetch result row
+  		if($numResultForm > 0) {
+		?>
+
+			<!--Table-->
+			<div class="table-responsive">          
+				<table class="table">
+				<thead>
+					<tr>
+					<th>ID</th>
+					<th>Title</th>
+					<th>Status</th>
+					<th>Created On</th>
+					<th id="operationHeader">Operation</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php 
+				//declaring count for index 
+				$count = 1;
+				while($row = mysqli_fetch_array($queryResultForm)){
+					//form data
+					$formId = $row[0];
+					$title = $row[1];
+					$competitionId = $row[2];
+					$status = $row[3];
+					$createdOn = $row[4];
+//competition data
+//$competitionStatus = $row[10];
+										
+					//Assign string to status 
+					if ($status == 'AVL'){
+						$status = "Available";
+					}
+					else if ($status == 'ONG'){						
+						$status = "Ongoing";
+					}
+					else if ($status == 'PEN') {
+						$status = "Pending";
+					}
+
+/*if ($competitionStatus = 'ONG') {
+$updateStatus = "Update form SET $"
+}*/
+														
+					//Display table data
+					echo "<tr>";
+					echo "<td>".$count++."</td>";
+					echo "<td>$title</td>";
+					echo "<td>$status</td>";
+					echo "<td>$createdOn</td>";
+					echo "<td>";
+					echo "<a id=\"viewButton\" href=\"\" class=\"btn btn-secondary col-xl-4\">View</a> ";
+					echo "<a id=\"updateButton\" href=\"\" class=\"btn btn-secondary col-xl-4\">Update</a> ";
+					echo "<button id=\"deleteButton\" type=\"button\" class=\"btn btn-secondary col-xl-4\">Delete</button> ";
+					echo "</td>";
+					echo "</tr>";
+					?>
+				</tbody>
+				</table>
+			</div> <!--Close table div-->
+								
+				<!--Button and pagination-->
+				<div class="row">
+					<a id="createItem" href="<?php echo 'discussion_create.php' ?>" class="btn btn-info mt-3">Create Form</a>
 					<ul class="pagination col-sm-4 mt-3 mx-auto">
 						<li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
 						<li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -103,30 +115,48 @@
 						<li class="page-item"><a class="page-link" href="#">3</a></li>
 						<li class="page-item"><a class="page-link" href="#">Next</a></li>
 					</ul>
-				</div>
-			
-			</div>
-			
-			
-			
-			
-		
-		</div>
-		
-		
-		<!--Footer-->
-		<?php include "footer.php"?>
-		
-	  
-	</div>
+				</div> <!--Close Button and pagination div-->
 
-	<script>
-		$(document).ready(function(){
-			$('[data-toggle="tooltip"]').tooltip(); 
-		});
-	</script>
-	
-  </body>
+				<?php
+				} //end while loop
+		?>
+		</div> <!--Close Card div--> 
+		<?php
+		} //end if numResultForm check 	
+
+
+		else {
+			?>
+				<!--Button-->
+				<div class="row">		
+					<a id="createItem" href="<?php echo 'discussion_create.php' ?>" class="btn btn-info mt-3">Create Form</a>
+				</div> 
+ 
+			<?php
+  		}	 //end else numResultForm check
+
+	}// end numResultCompetition check
+} //end isset
+else{
+	?>
+	<div class="text-center">
+		<h1 >No Data Available</h1>
+		<a href="<?php echo 'competition_manage.php' ?>" class="btn btn-info mt-3">Back</a>
+	</div> 
+	<?php
+}
+  ?>	
   
-  
+  <!--Footer-->
+	<?php include "footer.php"?>
+
+</div> <!--Close whole page div-->   
+
+<script>
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip(); 
+	});
+</script>
+
+</body>
 </html>
--- scripts/competition_manage.php
+++ scripts/competition_manage.php
@@ -1,4 +1,6 @@
-	<?php include "head.php"?>
+<?php 
+include "head.php"
+?>
 	
 	<body>
   
@@ -9,25 +11,32 @@
 		
 		<br><br>
 		
-		
 		<!--Card-->
 		<div class="container">
-			
+			<?php 
+				//Create sql statement
+				$query = "SELECT * FROM competition";
+				//execute query
+				$queryResult = mysqli_query($conn, $query) or die (mysqli_connect_error());
+				//check result rows
+				$numResult = mysqli_num_rows($queryResult);
+
+			?>
+
 			<h1> Manage Competition</h1>
 			
-			<ul class="breadcrumb">
+			<ul class="breadcrumb mb-0">
 			  <li class="breadcrumb-item"><a href="<?php echo 'home.php' ?>">Home</a></li>
 			  <li class="breadcrumb-item active">Manage Competition</li>
 			</ul>
 			
 			
 			<div class="container">
-				
 				<div class="table-responsive">          
 					<table class="table">
 						<thead>
 						  <tr>
-							<th>ID</th>
+						  	<th>No</th>
 							<th>Title</th>
 							<th>Start Date</th>
 							<th>End Date</th>
@@ -38,57 +47,60 @@
 						  </tr>
 						</thead>
 						<tbody>
-						  <tr>
-							<td>1</td>
-							<td>10-14 Age</td>
-							<td>2017-11-21</td>
-							<td>2017-11-28</td>
-							<td>Ongoing</td>
-							<td>Yes</td>
-							<td>2017-10-24 23:11:05</td>
-							<td>
-								<a id="viewButton" href="<?php echo 'competition_form_manage.php' ?>" class="btn btn-secondary col-xl-4 text-center">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-						<tbody>
-						  <tr>
-							<td>2</td>
-							<td>15-16 Age</td>
-							<td>2017-11-29</td>
-							<td>2017-12-06</td>
-							<td>Available</td>
-							<td>Yes</td>
-							<td>2017-10-24 12:00:45</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
-						<tbody>
-						  <tr>
-							<td>3</td>
-							<td>17-18 Age</td>
-							<td>2017-12-07</td>
-							<td>2017-12-14</td>
-							<td>Available</td>
-							<td>No</td>
-							<td>2017-10-24 11:04:09</td>
-							<td>
-								<a id="viewButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">View</a>
-								<a id="updateButton" href="<?php echo '' ?>" class="btn btn-secondary col-xl-4">Update</a>
-								<button id="deleteButton" type="button" class="btn btn-secondary col-xl-4">Delete</button>
-							</td>
-						  </tr>
-						</tbody>
+						<?php 
+						//fetch result row
+						if($numResult > 0) {
+							//declaring count for index 
+							$count = 1;
+							while($row = mysqli_fetch_assoc($queryResult)){
+								$competitionId = $row["competitionId"];
+								$title = $row["title"];
+								$startDate = $row["startDate"];
+								$endDate = $row["endDate"];
+								$status = $row["status"];	
+								$display = $row["display"];				
+								$createdOn = $row["createdOn"];				
+
+								//Assign string to display 
+								if ($display == "Y"){
+									$display = "Yes";
+								}
+								else if ($display == 'N'){
+									$display = "No";
+								}
+
+								//Assign string to status 
+								if ($status == 'AVL'){
+									$status = "Available";
+								}
+								else if ($status == 'ONG'){						
+									$status = "Ongoing";
+								}
+								else if ($status == 'PEN') {
+									$status = "Pending";
+								}
+
+								//Display table data
+								echo "<tr>";
+								echo "<td>".$count++."</td>";
+								echo "<td>$title</td>";
+								echo "<td>$startDate</td>";
+								echo "<td>$endDate</td>";
+								echo "<td>$status</td>";
+								echo "<td>$display</td>";
+								echo "<td>$createdOn</td>";
+								echo "<td>";
+									echo "<a id=\"viewButton\" href=\"competition_form_manage.php?id=$competitionId\" class=\"btn btn-secondary col-xl-4 text-center\">View</a> ";
+									echo "<a id=\"updateButton\" href=\"\" class=\"btn btn-secondary col-xl-4\">Update</a> ";
+									echo "<button id=\"deleteButton\" type=\"button\" class=\"btn btn-secondary col-xl-4\">Delete</button> ";
+								echo "</td>";
+								echo "</tr>";
+
+							}
+						 } 
+						 ?>
+					 </tbody>
 					</table>
-					
-				
-					
 				</div>
 			
 				
